83840df66bc0604380fcea13862ed653dbbf1063
Javier C <youracow@gmail.com>
2015-10-27 14:39:56 -0600

    added resources for comments

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments, shallow: true
   end
-  # resources :comments
+  resources :comments
   # resources :comments
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
