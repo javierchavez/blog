efe8db14174811c07ec20c3b95a932f9e1740cff
Javier C <youracow@gmail.com>
2015-10-27 16:05:29 -0600

    redirect to post comments path after destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -56,9 +56,11 @@ class CommentsController < ApplicationController
   # DELETE /comments/1
   # DELETE /comments/1.json
   def destroy
+    post = Post.find(@comment.post.id)
+
     @comment.destroy
     respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
+      format.html { redirect_to post_comments_path(post), notice: 'Comment was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
