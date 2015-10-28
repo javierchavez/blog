cfb038d1b60c900061ccdf50e34094837d172f05
Javier C <youracow@gmail.com>
2015-10-27 23:11:15 -0600

    updated for user auth

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
+  belongs_to :user
   validates :body, presence: true
   validates :post_id, presence: true
   scope :desc, -> { order(created_at: :desc) }
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ActiveRecord::Base
+  belongs_to :user
   has_many :comments, dependent: :destroy
   validates :title, presence: true
   validates :body, presence: true
+  validates :user_id, presence: true
   scope :desc, -> { order(created_at: :desc) }
 end
